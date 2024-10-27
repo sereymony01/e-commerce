ed72d83a228b51c6f9f4c4586c143c37552cda25
Sokmeak <sarensokmeak721@gmail.com>
2024-10-27 22:23:45 +0700

    add button as a component

---
+++ src/Components/ShopButton.vue
@@ -0,0 +1,44 @@
+<template>
+   <button :style="{ backgroundColor: bgBtnColor }" @click="HandleClick ">{{ label }}<svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 12H5m14 0-4 4m4-4-4-4"/>
+</svg>
+</button>
+
+</template>
+
+
+<script>  
+export default{
+    props:{
+        bgBtnColor: String,
+        label: String,
+    },
+
+    methods:{
+        HandleClick(){
+            alert("Redirecting to shopping...");
+        }
+    }
+
+}
+
+
+</script>
+
+<style scoped>
+
+button {
+  width: 50%;
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+ justify-items: center;
+  padding: 8px 16px;
+  color: white;
+  /* background-color: #36b37e; */
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+</style>
