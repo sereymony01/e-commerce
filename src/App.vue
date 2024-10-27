ab27272478974a8044bb69442bb1975a010ce4e5
SARENSokmeak <saroussokmeak721@gmail.com>
2024-10-27 09:50:37 +0700

    add image for e-commerce

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,63 @@
-<script setup>
-
-</script>
-
 <template>
+  <!-- Category List -->
 
-  This is my first VueJS project.
- 
+  <div class="product-list">
+    <product-category
+      v-for="(product, index) in products"
+      :key="index"
+      :title="product.title"
+      :items="product.items"
+      :imageSrc="product.imageSrc" />
+  </div>
 
+  <!--Promotion List  -->
 
   <RouterView />
 </template>
 
-<style scoped>
+<script>
+import ProductCategory from "./Components/ProductCategory.vue";
 
+export default {
+  components: {
+    ProductCategory,
+  },
+
+  data() {
+    return {
+      products: [
+        { title: "Buger", items: 14, imageSrc: "@/assets/Hamburger.svg" },
+        { title: "Peach", items: 17, imageSrc: "@/assets/Peach.svg" },
+        { title: "Organic Kiwi", items: 21, imageSrc: "@/assets/Kiwi.svg" },
+        { title: "Red Apple", items: 25, imageSrc: "@/assets/RedApple.svg" },
+        { title: "Snack", items: 34, imageSrc: "@/assets/snacks.svg" },
+        { title: "Black Plum", items: 25, imageSrc: "@/assets/BlackPulm.svg" },
+        { title: "Vegetables", items: 63, imageSrc: "@/assets/Vegetable1.svg" },
+        { title: "Headphone", items: 53, imageSrc: "@/assets/HeadPhone.svg" },
+        {
+          title: "Cake and Milk",
+          items: 53,
+          imageSrc: "./assets/CakeAndMilk.avg",
+        },
+
+        { title: "Orange", items: 63, imageSrc: "./assets/Orange.svg" },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.product-list {
+  display: flex;
+  overflow-x: auto;
+  padding: 20px;
+  background-color: #333;
+}
+.product-image {
+  width: 80px;
+  height: 80px;
+  object-fit: cover;
+  margin-bottom: 8px;
+}
 </style>
---
+++ src/Components/ProductCategory.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="product-category">
+    <img :src="imageSrc" :alt="title" class="project-image" />
+    <h3>{{ title }}</h3>
+    <p>
+      {{ items }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    items: Number,
+    imageSrc: String,
+  },
+};
+</script>
+<style scoped>
+.product-category {
+  text-align: center;
+  width: 120px;
+  padding: 10px;
+  background-color: #f0f2f5;
+  border-radius: 10px;
+  margin: 10px;
+}
+.product-image {
+  width: 80px;
+  height: 80px;
+  object-fit: cover;
+  margin-bottom: 8px;
+}
+</style>
